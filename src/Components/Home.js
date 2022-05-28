e5be23ad92c10c74cad658a45b63cf2a93ca317c
ln-dev7 <leonelngoya@gmail.com>
2022-05-28 17:02:56 +0100

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,8 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     const deleteBlog = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id)
@@ -12,19 +14,39 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch('https://localhost:8080/blogs')
+        fetch('http://localhost:8000/blogs')
             .then((response) => {
-                return response.json()
+                if (response.ok){
+                    setError(false)
+                    return response.json()
+                }
+                else {
+                    setLoading(false)
+                    setError(true)
+                }
             })
             .then((data) => {
+                setLoading(false)
+                setError(false)
                 setBlogs(data)
+            })
+            .catch(err => {
+                setLoading(false)
+                setError(true)
+                console.log(err.message)
             })
     }, [])
 
     return (
         <div className="home">
             {
-                blogs ? <BlogList blogs={blogs} deleteBlog={deleteBlog} title={'Liste des blogs'}/> : <div className="loading"></div>
+                error && <h2>Une erreur s'est produite ...</h2>
+            }
+            {
+                loading && <div className="loading"></div>
+            }
+            {
+                blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog} title={'Liste des blogs'}/>
             }
         </div>
     );
