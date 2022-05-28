145f472283a6dad463ff89dfc1ce297c92ffefb7
ln-dev7 <leonelngoya@gmail.com>
2022-05-28 21:09:25 +0100

    creation du formulaire d' ajout de l' article

--- src/Pages/AddArticle.js
+++ src/Pages/AddArticle.js
@@ -1,7 +1,28 @@
+import '../Styles/AddArticle.css'
 const AddArticle = () => {
     return (
-        <h2>Ajouter</h2>
-    );
+        <div className="add-article">
+            <form>
+                <div className="article-input">
+                    <label htmlFor="title">Titre de l' article</label>
+                    <input type="text" id="title" />
+                </div>
+                <div className="article-input">
+                    <label htmlFor="author">Nom de l' auteur</label>
+                    <input type="text" id="author" />
+                </div>
+                <div className="article-input">
+                    <label htmlFor="content">Contenu de l' article</label>
+                    <textarea id="content" rows="3" />
+                </div>
+                <div className="article-input">
+                    <label htmlFor="image">Lien de l' image</label>
+                    <input type="text" id="image" value="https://picsum.photos/500/300" placeholder="https://picsum.photos/500/300" />
+                </div>
+                <button type="submit">Ajouter l' article</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddArticle;
